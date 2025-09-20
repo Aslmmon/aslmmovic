3b28f7f7f6550da4e77f10facffa08f0686a7787
Aslm <aslmmon993@gmail.com>
2025-09-20 17:36:57 -0300

    update website to new portfolio website

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"18b71d647a292a980abb405ac7d16fe1f0b204
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3679793635"
+    serviceWorkerVersion: "2501099471"
   }
 });
